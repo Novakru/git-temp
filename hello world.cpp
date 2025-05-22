af9224d22836d13369f36ef85fc3b56499f73157
jiangjiahao0208 <1748492421@qq.com>
2025-05-22 21:57:49 +0800

    Revert "R5"
    This reverts commit ace823832a7ebdb5b3e1a8abc614181e504899b8.

--- hello world.cpp
+++ hello world.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b) {
-    return a + b;
-}
-
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "sum(1, 2) = " << sum(1, 2) << endl;
     return 0;
 }
 
